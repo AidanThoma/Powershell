c73ea1d4427a8b4d2d126597334035277093148b
Aidan Thoma (Elevated) <aidan.thoma@drake.edu>
2025-06-27 13:49:59 -0500

    Updated Writing.ps1 so it reads the bible and the colors are now random

--- Scripts/Writing.ps1
+++ Scripts/Writing.ps1
@@ -1,9 +1,23 @@
-$fileContent = Get-Content .\Files\Writing.txt
+$fileContent = Get-Content .\Files\Bible.txt
 
-$colors = @('Blue', 'Green', 'Cyan', 'Red', 'Magenta', 'Yellow', 'White')
+$colors = @(
+"DarkBlue",
+"DarkGreen",
+"DarkCyan",
+"DarkRed",
+"DarkMagenta",
+"DarkYellow",
+"Gray",
+"DarkGray",
+"Blue",
+"Green",
+"Cyan",
+"Red",
+"Magenta",
+"Yellow",
+"White")
 
 $num = 0
-$loops = 0
 
 <#
 foreach ($line in $fileContent){
@@ -20,6 +34,9 @@ Write-Host "Loops: $($loops)`n Num: $($num)"
 
 $words = $fileContent -split '\s+' # This splits it by any white space
 
+
+#   Change color periodicly
+<#
 foreach ($word in $words) {
     Write-Host "$($word) " -NoNewline -ForegroundColor $colors[$num]
     $num++
@@ -30,5 +47,17 @@ foreach ($word in $words) {
     if ($num -ge $colors.Length) {
         $num = 0
     }
-    Start-Sleep -Milliseconds 50
+    Start-Sleep -Milliseconds 10
+}
+#>
+
+# Changes color 'randomly'
+foreach ($word in $words) {
+    Write-Host "$($word) " -NoNewline -ForegroundColor $colors[$num]
+    if ($word -like "*.*"){
+        Write-Host "`n"
+    }
+
+    $num = Get-Random -Maximum ($colors.Length)
+    Start-Sleep -Milliseconds 10
 }
